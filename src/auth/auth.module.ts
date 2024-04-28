77f9d5b6225856745555c4ffea5f1332399fecdf
al0nepy <al0nepy@protonmail.com>
2024-04-28 14:55:56 +0300

    JWT auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,20 +1,23 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { jwtConfig } from './constants';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
     UserModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       global: true,
       secret: jwtConfig.secret,
-      signOptions: { expiresIn: '7200s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
